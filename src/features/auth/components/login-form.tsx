9f6b68a1540b8c28c352e904aeeef2d6e6f2ed2e
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-29 21:44:42 +0300

    feat: login form and functionality

---
+++ src/features/auth/components/login-form.tsx
@@ -0,0 +1,115 @@
+import { useForm } from 'react-hook-form'
+import { isAxiosError } from 'axios'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQueryClient } from '@tanstack/react-query'
+
+import type { LoginValues } from '@/features/auth/utils/auth.types'
+import { loginSchema } from '@/features/auth/utils/schema'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { PasswordInput } from '@/components/custom/password-input'
+import { Button } from '@/components/ui/button'
+import { useMutate } from '@/hooks/use-mutate'
+import axios from '@/lib/api/axios'
+import { handleApiErrors, mutationErrorHandler } from '@/lib/error-handlers'
+import { CustomAlert } from '@/components/custom/custom-alert'
+import { useError } from '@/hooks/use-error'
+import { router } from '@/main'
+
+export function LoginForm() {
+  const form = useForm<LoginValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(loginSchema),
+  })
+  const { clearErrors, errors, onError } = useError()
+  const queryClient = useQueryClient()
+
+  const { isPending, mutate } = useMutate(async (data: LoginValues) => {
+    await axios('/sanctum/csrf-cookie')
+    await axios.post('/api/login', data)
+  })
+  return (
+    <div className="space-y-4">
+      {errors && <CustomAlert variant="error" description={errors} />}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((data) => {
+            clearErrors()
+            mutate(data, {
+              onSuccess: () => {
+                form.reset()
+                queryClient.invalidateQueries({ queryKey: ['auth-user'] })
+                router.invalidate()
+                router.navigate({ to: '/', replace: true })
+              },
+              onError: (error) => {
+                if (isAxiosError(error)) {
+                  if (error.response?.status === 422) {
+                    handleApiErrors(error.response.data.errors, form.setError)
+                  } else {
+                    onError(mutationErrorHandler(error))
+                  }
+                }
+              },
+            })
+          })}
+          className="space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    {...field}
+                    placeholder="jsmith@example.com"
+                    disabled={isPending}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordInput
+                    {...field}
+                    placeholder="*******"
+                    disabled={isPending}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full"
+            size="lg"
+            disabled={isPending}
+          >
+            Sign In
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
---
+++ src/routes/(auth)/login.tsx
@@ -0,0 +1,28 @@
+import { createFileRoute } from '@tanstack/react-router'
+import logo from '@/assets/atarah_logo.png'
+
+import { LoginForm } from '@/features/auth/components/login-form'
+
+export const Route = createFileRoute('/(auth)/login')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Atarah solutions logo"
+          src={logo}
+          className="mx-auto h-32 w-auto"
+        />
+        <h2 className="-mt-6 text-center text-2xl/9 font-bold tracking-tight">
+          Sign in to your account
+        </h2>
+      </div>
+      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
