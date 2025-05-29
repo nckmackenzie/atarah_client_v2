5d068736b64891ff153281c0260c36be0256d1ce
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-29 11:45:34 +0300

    feat: app setup

---
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 30 } },
+})
+
+export function getContext() {
+  return {
+    queryClient,
+  }
+}
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
---
+++ src/types/index.types.ts
@@ -0,0 +1,42 @@
+export type UserType = 'admin' | 'user'
+
+export interface User {
+  id: number
+  contact: string
+  name: string
+  email: string | null
+  userType: UserType
+}
+
+export interface WithId {
+  id: string
+}
+
+export interface WithName {
+  name: string
+}
+
+export interface WithIdAndName extends WithId, WithName {}
+
+export interface IsEdit {
+  isEdit?: boolean
+}
+
+export type IsEditRequired = Required<IsEdit>
+
+export interface IsPending {
+  isPending: boolean
+}
+
+export type PaymentMethod = 'cash' | 'mpesa' | 'cheque' | 'bank'
+
+export interface Form extends WithName {
+  id: number
+  module: string
+  path: string
+}
+
+export interface Option {
+  value: string
+  label: string
+}
