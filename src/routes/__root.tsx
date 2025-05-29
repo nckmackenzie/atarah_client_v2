d1dfdd31eade1995f3b06e5b53a4730262feea8a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-29 22:48:21 +0300

    style: remove container

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,26 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Toaster } from 'sonner'
 import type { QueryClient } from '@tanstack/react-query'
 
 import TanStackQueryLayout from '@/integrations/tanstack-query/layout.tsx'
+import { userQueryOptions } from '@/features/auth/utils/query-options'
 
 interface MyRouterContext {
   queryClient: QueryClient
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
+  beforeLoad: async ({ context }) => {
+    const data = await context.queryClient.ensureQueryData(userQueryOptions())
+    return { user: data }
+  },
   component: () => (
     <>
-      <div>
+      <main className="h-dvh">
         <Outlet />
-      </div>
+        <Toaster />
+      </main>
       <TanStackRouterDevtools />
 
       <TanStackQueryLayout />
