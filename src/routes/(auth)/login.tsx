56cc8b3a156c92e37689ef58eab84874dea90a9a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-30 08:15:34 +0300

    fix: redirect user to dashboard if they are authenticated

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,13 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import logo from '@/assets/atarah_logo.png'
 
 import { LoginForm } from '@/features/auth/components/login-form'
+import { useDocumentTitle } from '@/hooks/use-title'
+import { useAuth } from '@/hooks/use-auth'
 
 export const Route = createFileRoute('/(auth)/login')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  useDocumentTitle('Login')
+  const navigate = useNavigate()
+  const { isAuthenticated, isLoading } = useAuth()
+
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      navigate({
+        to: '/dashboard',
+        replace: true,
+      })
+    }
+  }, [isAuthenticated, isLoading, navigate])
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
