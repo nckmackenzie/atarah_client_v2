521f08b14e429908cb13c36d44caed92c38c8e26
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-01 18:37:28 +0300

    refactor: use new pageheader props

--- src/routes/_authenticated/roles/index.tsx
+++ src/routes/_authenticated/roles/index.tsx
@@ -1,8 +1,6 @@
-import { Link, createFileRoute, useNavigate } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { useSuspenseQuery } from '@tanstack/react-query'
-import { Plus } from 'lucide-react'
 import PageHeader from '@/components/custom/page-header'
-import { Button } from '@/components/ui/button'
 import { useDocumentTitle } from '@/hooks/use-title'
 import { roleQueryOptions } from '@/features/admin/services/query-options'
 import { searchValidateSchema } from '@/lib/schema-rules'
@@ -32,14 +30,13 @@ function RolesRouteComponent() {
   } = useSuspenseQuery(roleQueryOptions.all(q))
   return (
     <div className="space-y-6">
-      <PageHeader title="Roles" description="Manage your roles here.">
-        <Button size="lg" asChild className="w-full sm:w-auto">
-          <Link to="/roles/new" className="flex items-center gap-x-2">
-            <Plus />
-            <span>Create Role</span>
-          </Link>
-        </Button>
-      </PageHeader>
+      <PageHeader
+        title="Roles"
+        description="Manage your roles here."
+        buttonText="Create Role"
+        path="/roles/new"
+      />
+
       <Search
         defaultValue={q}
         placeholder="Search by role name"
