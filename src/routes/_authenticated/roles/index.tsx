f2cf21d3c3246caf8dfed7238792931530d9b506
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-01 15:37:48 +0300

    feat: roles index page

---
+++ src/routes/_authenticated/roles/index.tsx
@@ -0,0 +1,59 @@
+import { Link, createFileRoute, useNavigate } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Plus } from 'lucide-react'
+import PageHeader from '@/components/custom/page-header'
+import { Button } from '@/components/ui/button'
+import { useDocumentTitle } from '@/hooks/use-title'
+import { roleQueryOptions } from '@/features/admin/services/query-options'
+import { searchValidateSchema } from '@/lib/schema-rules'
+import { FormLoader } from '@/components/custom/loaders'
+import { CustomAlert } from '@/components/custom/custom-alert'
+import Search from '@/components/custom/search'
+import { Rolesdatatable } from '@/features/admin/components/roles/roles-datatable'
+
+export const Route = createFileRoute('/_authenticated/roles/')({
+  validateSearch: searchValidateSchema,
+  loaderDeps: ({ search: { q } }) => ({ q }),
+  loader: async ({ context, deps: { q } }) =>
+    await context.queryClient.ensureQueryData(roleQueryOptions.all(q)),
+  component: RolesRouteComponent,
+  pendingComponent: () => <FormLoader />,
+  errorComponent: ({ error }) => (
+    <CustomAlert variant="error" description={error.message} />
+  ),
+})
+
+function RolesRouteComponent() {
+  useDocumentTitle('Roles')
+  const { q } = Route.useSearch()
+  const navigate = useNavigate({ from: Route.fullPath })
+  const {
+    data: { data: roles },
+  } = useSuspenseQuery(roleQueryOptions.all(q))
+  return (
+    <div className="space-y-6">
+      <PageHeader title="Roles" description="Manage your roles here.">
+        <Button size="lg" asChild className="w-full sm:w-auto">
+          <Link to="/roles/new" className="flex items-center gap-x-2">
+            <Plus />
+            <span>Create Role</span>
+          </Link>
+        </Button>
+      </PageHeader>
+      <Search
+        defaultValue={q}
+        placeholder="Search by role name"
+        parentClassName="w-full md:w-1/2"
+        onHandleSearch={(value) =>
+          navigate({
+            search: (prev) => ({
+              ...prev,
+              q: value.trim().length > 0 ? value : undefined,
+            }),
+          })
+        }
+      />
+      <Rolesdatatable roles={roles} />
+    </div>
+  )
+}
