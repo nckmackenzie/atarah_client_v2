bdc75a781071e3575b9330a2ebbf93dcdc442345
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-03 15:13:51 +0300

    feat: add util functions for crud operations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { queryOptions } from '@tanstack/react-query'
 import { clsx } from 'clsx'
 import { toast } from 'sonner'
 import { twMerge } from 'tailwind-merge'
@@ -5,6 +6,8 @@ import type { Option, TRoutes } from '@/types/index.types'
 import type { ClassValue } from 'clsx'
 import { getContext } from '@/integrations/tanstack-query/root-provider'
 import { router } from '@/main'
+import { mutationErrorHandler } from '@/lib/error-handlers'
+import axios from '@/lib/api/axios'
 
 export const PAYMENT_METHOD: Array<Option> = [
   {
@@ -59,13 +62,63 @@ export function searchParamsToObject(
 export function successHandler(
   isEdit: boolean,
   invalidateKey: Array<string>,
-  redirectTo: TRoutes,
-  resource: string,
+  redirectTo?: TRoutes,
+  resource?: string,
 ) {
   const { queryClient } = getContext()
   toast.success(`${isEdit ? 'Updated' : 'Created'} ${resource} successfully!`)
   queryClient.invalidateQueries({
     queryKey: invalidateKey,
   })
+  if (!redirectTo) return
   router.navigate({ to: redirectTo })
+}
+
+export function createListQuery<T>(resource: string, endpoint: string) {
+  return (q?: string) =>
+    queryOptions({
+      queryKey: [resource, { q }],
+      queryFn: async (): Promise<{ data: Array<T> }> => {
+        const params = searchParamsToObject({ q })
+        try {
+          const { data } = await axios(`${endpoint}?${params.toString()}`)
+          return data
+        } catch (err) {
+          const error = mutationErrorHandler(err)
+          throw new Error(error)
+        }
+      },
+    })
+}
+
+export function createDetailQuery<T>(resource: string, endpoint: string) {
+  return (id: string) =>
+    queryOptions({
+      queryKey: [resource, id],
+      queryFn: async (): Promise<{ data: T }> => {
+        try {
+          const { data } = await axios(`${endpoint}/${id}`)
+          return data
+        } catch (err) {
+          const error = mutationErrorHandler(err)
+          throw new Error(error)
+        }
+      },
+    })
+}
+
+export async function createResource<T>(resource: string, values: T) {
+  return await axios.post(`/api/${resource}`, values)
+}
+
+export async function updateResource<T>(
+  resource: string,
+  id: string,
+  values: T,
+) {
+  return await axios.put(`/api/${resource}/${id}`, values)
+}
+
+export async function deleteResource(resource: string, id: string) {
+  return await axios.delete(`/api/${resource}/${id}`)
 }
