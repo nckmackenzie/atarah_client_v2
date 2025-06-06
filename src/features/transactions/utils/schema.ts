e0a0a0d6ddfe4fb8b13eff6c98ebf14eb575e48e
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-06 19:10:42 +0300

    feat: schema for invoice payment

--- src/features/transactions/utils/schema.ts
+++ src/features/transactions/utils/schema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod'
 import {
+  optionalNumberSchemaEntry,
   optionalStringSchemaEntry,
+  paymentMethodSchemaEntry,
+  paymentReferenceSchemaEntry,
+  requiredDateSchemaEntry,
+  requiredNumberSchemaEntry,
   requiredStringSchemaEntry,
 } from '@/lib/schema-rules'
 
@@ -19,6 +24,69 @@ export const accountsFormSchema = z
         code: 'custom',
         message: 'Parent Account is required for sub category',
         path: ['parentId'],
+      })
+    }
+  })
+
+export const invoiceFormSchema = z
+  .object({
+    invoiceNo: requiredStringSchemaEntry('Invoice number is required'),
+    invoiceDate: requiredDateSchemaEntry(),
+    dueDate: requiredDateSchemaEntry(),
+    clientId: requiredStringSchemaEntry('Client is required'),
+    terms: requiredStringSchemaEntry('Terms is required'),
+    vatType: z.enum(['no_vat', 'inclusive', 'exclusive'], {
+      required_error: 'Vat type is required',
+    }),
+    vat: z.coerce.number().optional(),
+    items: z.array(
+      z.object({
+        id: z.string(),
+        serviceId: requiredStringSchemaEntry('Service is required'),
+        quantity: requiredNumberSchemaEntry('Quantity is required'),
+        rate: requiredNumberSchemaEntry('Rate is required'),
+        discount: optionalNumberSchemaEntry(),
+      }),
+    ),
+  })
+  .superRefine(({ vatType, vat, dueDate, invoiceDate }, ctx) => {
+    if (vatType !== 'no_vat' && !vat) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Vat is required',
+        path: ['vat'],
+      })
+    }
+    if (dueDate < invoiceDate) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Due date must be greater than invoice date',
+        path: ['dueDate'],
+      })
+    }
+  })
+
+export const invoicePaymentFormSchema = z
+  .object({
+    invoiceId: requiredStringSchemaEntry('Invoice is required'),
+    paymentDate: requiredDateSchemaEntry(),
+    amount: requiredNumberSchemaEntry('Amount is required'),
+    paymentMethod: paymentMethodSchemaEntry(),
+    paymentReference: paymentReferenceSchemaEntry(),
+  })
+  .superRefine(({ paymentDate, invoiceId }, ctx) => {
+    if (!invoiceId) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Invoice ID is required',
+        path: ['invoiceId'],
+      })
+    }
+    if (paymentDate > new Date()) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Payment date cannot be in the future',
+        path: ['paymentDate'],
       })
     }
   })
