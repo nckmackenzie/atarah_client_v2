3ffd0d95e7472be9b16a52b4ec7f838022b5e3ef
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-09 13:23:46 +0300

    feat: add types for invoices

--- src/features/transactions/utils/transactions.types.ts
+++ src/features/transactions/utils/transactions.types.ts
@@ -1,6 +1,10 @@
 import type { z } from 'zod'
 import type { WithCreatedAt, WithId, WithIdAndName } from '@/types/index.types'
-import type { accountsFormSchema } from '@/features/transactions/utils/schema'
+import type {
+  accountsFormSchema,
+  invoiceFormSchema,
+  invoicePaymentFormSchema,
+} from '@/features/transactions/utils/schema'
 
 type AccountType =
   | 'income'
@@ -29,3 +33,34 @@ export type AccountMin = WithIdAndName &
   }
 export type AccountFormValues = z.infer<typeof accountsFormSchema>
 export type AccountValue = WithId & AccountFormValues
+
+export interface Invoice extends WithId, WithCreatedAt {
+  invoiceDate: string
+  invoiceNo: string
+  terms: '0' | '30' | '60'
+  dueDate: string
+  vatType: 'exclusive' | 'inclusive' | 'no_vat'
+  vat: string
+  discount: string
+  subTotal: string
+  vatAmount: string
+  totalAmount: string
+  updatedAt: string | null
+  amountPaid: string | null
+  client: WithIdAndName
+}
+
+export interface InvoiceWithDetails extends Invoice {
+  details: Array<{
+    id: string
+    remarks: string | null
+    quantity: number
+    rate: string
+    discount: string
+    amount: string
+    service: WithIdAndName
+  }>
+}
+
+export type InvoiceFormValues = z.infer<typeof invoiceFormSchema>
+export type InvoicePaymentFormValues = z.infer<typeof invoicePaymentFormSchema>
