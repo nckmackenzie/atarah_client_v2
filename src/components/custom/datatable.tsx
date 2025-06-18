2df722baa35a21416f5ffe2dcf8d3323066a018a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-18 14:02:10 +0300

    style: add magrin top

--- src/components/custom/datatable.tsx
+++ src/components/custom/datatable.tsx
@@ -126,7 +126,7 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
+    <div className="flex items-center justify-between px-2 mt-2">
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
