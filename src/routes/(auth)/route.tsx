9778149ab8d9ee31a2454d88387657e1ee600809
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-18 22:21:01 +0300

    feat: layout for auth pages

---
+++ src/routes/(auth)/route.tsx
@@ -0,0 +1,23 @@
+import { Outlet, createFileRoute } from '@tanstack/react-router'
+import logo from '@/assets/ATH.png'
+
+export const Route = createFileRoute('/(auth)')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Atarah solutions logo"
+          src={logo}
+          className="mx-auto h-16 w-auto"
+        />
+      </div>
+      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
