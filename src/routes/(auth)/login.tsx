938ebab03c3123c3127c5358c9b35a278254a2f8
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-18 22:20:44 +0300

    refactor: change after auth layout

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import logo from '@/assets/atarah_logo.png'
 
 import { LoginForm } from '@/features/auth/components/login-form'
 import { useDocumentTitle } from '@/hooks/use-title'
 import { useAuth } from '@/hooks/use-auth'
+import { ErrorNotification } from '@/components/custom/error-component'
 
 export const Route = createFileRoute('/(auth)/login')({
   component: RouteComponent,
+  errorComponent: ({ error }) => <ErrorNotification message={error.message} />,
 })
 
 function RouteComponent() {
@@ -24,20 +25,16 @@ function RouteComponent() {
     }
   }, [isAuthenticated, isLoading, navigate])
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          alt="Atarah solutions logo"
-          src={logo}
-          className="mx-auto h-32 w-auto"
-        />
-        <h2 className="-mt-6 text-center text-2xl/9 font-bold tracking-tight">
+    <>
+      <div className="space-y-0.5  mt-2 mb-6">
+        <h2 className="text-center text-2xl/9 font-bold tracking-tight">
           Sign in to your account
         </h2>
+        <p className="text-sm text-muted-foreground text-center">
+          Enter your credentials to access your account.
+        </p>
       </div>
-      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
+      <LoginForm />
+    </>
   )
 }
