6222ab8bd6264cdc4c5a12070e25672ce68737e3
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-18 16:11:21 +0300

    feat: components for chart and clients

---
+++ src/features/dashboard/components/top-clients.tsx
@@ -0,0 +1,145 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts'
+import type { ColumnDef } from '@tanstack/react-table'
+import type { TopClient } from '@/features/dashboard/utils/dashboard.types'
+import type { ChartConfig } from '@/components/ui/chart'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { dashboardQueryOptions } from '@/features/dashboard/services/query-options'
+import { numberFormat } from '@/lib/formatters'
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
+
+import { DataTable } from '@/components/custom/datatable'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export function TopClients() {
+  const {
+    data: { data: clients },
+  } = useSuspenseQuery(dashboardQueryOptions.topClients())
+
+  const columns: Array<ColumnDef<TopClient>> = [
+    {
+      accessorKey: 'name',
+      header: 'Client Name',
+      cell: ({ row }) => row.original.name,
+    },
+    {
+      accessorKey: 'invoices',
+      header: 'Invoices',
+      cell: ({ row }) => row.original.invoices,
+    },
+    {
+      accessorKey: 'revenue',
+      header: 'Total Revenue',
+      cell: ({ row }) => numberFormat(row.original.revenue),
+    },
+  ]
+
+  return (
+    <Card className="shadow-none col-span-3">
+      <CardHeader>
+        <CardTitle>Top Clients</CardTitle>
+        <CardDescription>
+          Top 5 based on the total amount invoiced
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <DataTable
+          columns={columns}
+          data={clients}
+          withPaginationButtons={false}
+        />
+      </CardContent>
+    </Card>
+  )
+}
+
+const chartConfig = {
+  revenue: {
+    label: 'Revenue',
+    color: 'var(--chart-success)',
+  },
+  expenses: {
+    label: 'Expenses',
+    color: 'var(--chart-error)',
+  },
+} satisfies ChartConfig
+
+export function RevenueExpenseChart() {
+  const {
+    data: { data },
+  } = useSuspenseQuery(dashboardQueryOptions.revenueVsExpenses())
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle>Revenue vs Expenses</CardTitle>
+        <CardDescription>
+          Monthly comparison of revenue and expenses (6 Months)
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pl-2">
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={data}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <Bar
+              dataKey="expenses"
+              stackId="a"
+              fill="var(--color-expenses)"
+              radius={[0, 0, 4, 4]}
+            />
+            <Bar
+              dataKey="revenue"
+              stackId="a"
+              fill="var(--color-revenue)"
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
+
+export function RevenueVsExpensesSkeleton() {
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle>
+          <Skeleton className="h-6 w-40" />
+        </CardTitle>
+        <CardDescription>
+          <Skeleton className="h-4 w-60" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pl-2">
+        <div className="w-full h-60">
+          <Skeleton className="w-full h-full" />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
