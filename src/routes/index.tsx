b9c00f26057acd9fc5c378ca07d2b45a3f73e8e0
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-06-19 19:55:36 +0300

    refactor: index page redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,43 @@
-import { createFileRoute } from '@tanstack/react-router'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
+import React from 'react'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { useAuth } from '@/hooks/use-auth'
-import { Button } from '@/components/ui/button'
+import logo from '@/assets/ATH.png'
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
+  component: App,
 })
 
-function RouteComponent() {
-  const { logout } = useAuth()
-  return <Button onClick={() => logout()}>Logout</Button>
+function App() {
+  const { isLoading, isAuthenticated } = useAuth()
+  const navigate = useNavigate()
+
+  React.useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      navigate({
+        to: '/login',
+
+        replace: true,
+      })
+    } else if (isAuthenticated && !isLoading) {
+      navigate({
+        to: '/dashboard',
+        replace: true,
+      })
+    }
+  }, [isAuthenticated, isLoading, navigate])
+
+  return (
+    <div className="text-center h-full flex flex-col items-center justify-center gap-4">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Atarah solutions logo"
+          src={logo}
+          className="mx-auto h-16 w-auto"
+        />
+      </div>
+      <p className="text-sm text-muted-foreground">
+        Please wait while we redirect you...
+      </p>
+    </div>
+  )
 }
